2fc8ae2e20199c737bf5302671863c7ff41ed8ac
Yigitklncr <yigitkilincer@gmail.com>
2022-11-11 18:46:22 +0300

    Mobile responsiveness has configured.

--- src/Js/mobileResponsive.js
+++ src/Js/mobileResponsive.js
@@ -1,8 +1,15 @@
 function displayNoneDiv(){
     document.getElementById("opps-logo").style.display = "none";
     document.getElementById("footer-logos").style.display = "none";
+    if(window.innerWidth > 1200) {
+        document.getElementById("body").style.overflow = "hidden!important";
+    }
+    else {
+        document.getElementById("body").style.overflow = "scroll";
+    }
 }
 function displayFlexDiv(){
     document.getElementById("opps-logo").style.display = "flex";
     document.getElementById("footer-logos").style.display = "flex";
+    document.getElementById("body").style.overflow = "hidden";
 }
